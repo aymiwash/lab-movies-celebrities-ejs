270fc27c83a3f70e20da9499d69c7bf3f64f605b
Emilie Luong <emi.luong@gmail.com>
2023-07-10 18:42:36 +0200

    iteration 6

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,35 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+
+// all your routes here
+
+
+
+router.get("/create", (req, res) => {
+  res.render("celebrities/new-celebrity");
+});
+
+router.post("/create", async (req, res) => {
+  const data = req.body;
+  try {
+    const newCelebrity = await Celebrity.create(data);
+    res.redirect("/celebrities");
+  } catch (error) {
+    console.log(error);
+    res.render("celebrities");
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const allCelebrities = await Celebrity.find();
+    console.log(allCelebrities);
+    res.render("celebrities/celebrities", {allCelebrities});
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+module.exports = router;
---
+++ routes/movies.routes.js
@@ -0,0 +1,15 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require('express').Router()
+const Movie = require("../models/Movie.model");
+
+// all your routes here
+
+router.get("/create", (req, res)=>{
+    res.render("movies/new-movie")
+
+
+})
+
+
+
+module.exports = router
